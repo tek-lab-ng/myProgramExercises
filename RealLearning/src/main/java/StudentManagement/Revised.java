bc6915d5fccc1ad80df768303cd697c04bef5b19
Gabriel <gojabriel85@gmail.com>
2026-07-31 12:35:21 +0100

    Continuation of learning progress

--- RealLearning/src/main/java/StudentManagement/Revised.java
+++ RealLearning/src/main/java/StudentManagement/Revised.java
@@ -1,8 +1,6 @@
 package StudentManagement;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.*;
 
 public class Revised {
 
@@ -12,18 +10,35 @@ public class Revised {
         ArrayList<String> student = new ArrayList<String>();
         student.add("Gabriel");
         student.add("Janet");
+        student.add("Abel");
 
         String[] caps = {"Joseph", "Caleb"};
 
        System.out.println( Collections.addAll(student, caps ));
-       System.out.println(student);
+       //System.out.println(student);
        Collections.sort(student);
        System.out.println(student);
 
-       new Revised().checkings(caps);
+       //system.out.println(student);
 
-       
-       
+        List<String> teacher = new ArrayList<String>();
+        teacher.add("Kemi");
+        teacher.add("Fred");
+        teacher.add("oemi");
+        teacher.add("Gabriel");
+
+        System.out.println(teacher);
+        Collections.sort(teacher);
+
+        List<String> t = Collections.synchronizedList(teacher);
+
+        String carrot = "flowed";
+       boolean value = carrot.matches("\\D+");
+       System.out.println(value);
+
+
+       //new Revised().checkings(caps);
+
     }
 
     public void checkings(String ...n){

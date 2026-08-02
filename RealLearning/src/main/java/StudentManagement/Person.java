92a60e8b7ed680be33d262c39704a4bc4a496364
Gabriel <gojabriel85@gmail.com>
2026-08-02 09:19:59 +0100

    Added the Illegal Exception Class to check age validity

--- RealLearning/src/main/java/StudentManagement/Person.java
+++ RealLearning/src/main/java/StudentManagement/Person.java
@@ -1,7 +1,10 @@
 package StudentManagement;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
+//The University Management Structure
 abstract class Person {
     private int id;
     private String name;
@@ -11,7 +14,11 @@ abstract class Person {
     public Person(int id, String name, int age, String email) {
             this.id = id;
             this.name = name;
-            this.age = age;
+            if (age >= 18){
+                this.age = age;
+            } else {
+                throw new InvalidAgeException("This user " + name+ " is not valid to be a student or staff, need to be 18 and above in age");
+            }
             this.email = email;
 
     }
@@ -45,6 +52,7 @@ abstract class Person {
 
 }
 
+//This is the Library Faculty class
 abstract class  LibraryMember  extends Person{
 
     private String libraryCardNumber;
@@ -63,6 +71,7 @@ abstract class  LibraryMember  extends Person{
     }
 }
 
+//This is the Student class
 class Students extends LibraryMember {
 
     private String course;
@@ -94,6 +103,7 @@ class Students extends LibraryMember {
 
 }
 
+//This is the Lecturer class
 class Lecturer extends LibraryMember {
 
     private String department;
@@ -135,50 +145,100 @@ class Lecturer extends LibraryMember {
                 + " and I teach " + getDepartment());
     }
 }
-
+//This is the Security class
 class Security extends Person {
+    private int gateNumber;
 
-    public Security(int id, String name, int age, String email) {
+    public Security(int id, String name, int age, String email, int gateNumber) {
         super(id, name, age, email);
+        this.gateNumber = gateNumber;
+    }
+
+    public void setGateNumber(int gateNumber){
+        this.gateNumber = gateNumber;
+    }
+
+    public int getGateNumber(){
+        return gateNumber;
+    }
+    @Override
+    public void introduce() {
+        System.out.println("Hi, I'm " + getName()
+                + " and I work at Gate " + getGateNumber());
+    }
+}
+//This is the Janitor class
+class Janitor extends Person {
+
+    private String cleanArea;
+
+    public Janitor(int id, String name, int age, String email, String cleanArea) {
+        super(id, name, age, email);
+        this.cleanArea = cleanArea;
+    }
+
+    public void setCleanArea(String cleanArea){
+        this.cleanArea = cleanArea;
+    }
+
+    public String getCleanArea(){
+        return cleanArea;
     }
 
     @Override
     public void introduce() {
         System.out.println("Hi, I'm " + getName()
-                + " and I work at Gate 2.");
-    }
-}
-
-class Janitor extends Person {
-
-    public Janitor(int id, String name, int age, String email) {
-        super(id, name, age, email);
-    }
-
-    @Override
-    public void introduce() {
-
+                + " and I clean on the " + getCleanArea());
     }
 }
 
 
 
 class MyApp {
+
+    static List<Person> people;
+
     public static void main(String[] args) {
+
+        people = new ArrayList<>();
+
         Students s1 = new Students(1, "Gabriel", 20, "Computer Science", "010", "gabrieljames85@gmail.com" );
-        Students s2 = new Students(2, "Jayden", 17, "Chemistry", "011", "jayden@gmail.com" );
+        try{
+            Students s2 = new Students(2, "Jayden", 17, "Chemistry", "011", "jayden@gmail.com" );
+            people.add(s2);
+        }catch (InvalidAgeException e){
+            System.out.println(e.getMessage());
+        }
 
         Lecturer l1 = new Lecturer(1, "Fred", 40, "Computer Science", "012", "fred@gmail.com", 10000);
         Lecturer l2 = new Lecturer(2, "Lucy", 45, "Physics", "012", "Lucy@gmail.com", 20000);
 
-        Security sec1 = new Security(3, "Mike", 28, "Mike@gmail.com");
-        Security sec2 = new Security(4, "Ben", 29, "Ben@gmail.com");
+        Security sec1 = new Security(3, "Mike", 28, "Mike@gmail.com", 1);
+        Security sec2 = new Security(4, "Ben", 29, "Ben@gmail.com", 2);
 
 
-        Janitor jan1 = new Janitor(5, "Kemi", 30, "Kemi@gmail.com");
-        Janitor jan2 = new Janitor(6, "Aunty", 28, "Aunty@gmail.com");
+        Janitor jan1 = new Janitor(5, "Kemi", 30, "Kemi@gmail.com", "First Floor");
+        Janitor jan2 = new Janitor(6, "Aunty", 28, "Aunty@gmail.com", "Second Floor");
 
+        people.add(s1);
+        people.add(l1);
+        people.add(l2);
+        people.add(sec1);
+        people.add(sec2);
+        people.add(jan1);
+        people.add(jan2);
 
+        for (Person person : people){
+            person.introduce();
+        }
 
+    }
+
+}
+
+//Creating the InvalidAgeException Class to handle wrong age input
+class InvalidAgeException extends RuntimeException{
+    public InvalidAgeException(String e){
+        super(e);
     }
 }
